15c00848d2da33e7d3702a3219f6ef78d154e87f
kyusak0 <chirkinvova4@gmail.com>
2025-10-23 18:34:10 +0300

    init commit

---
+++ front/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import mainPage from "./pages/mainPage";
+
+
+export default function Home() {
+  return (<>
+  <Header />
+  <h1>
+      123
+    </h1>
+  </>
+    
+    
+  );
+}
---
+++ front/app/pages/mainPage.tsx
@@ -0,0 +1,7 @@
+import mainLayout from "../layouts/mainLayout";
+
+export default function mainPage() {
+    return (
+        mainLayout
+        )
+};

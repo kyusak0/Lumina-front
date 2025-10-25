33f40f6b56d3f5cf46894fcb30d75012453789b0
Saidosik <Sadiliofimperia@gmail.com>
2025-10-25 13:58:35 +0300

    Said dev api patch
    Рвботатю над апи ничего не работает, запушил чтоб была актуальная версия

--- app/api/_api.tsx
+++ app/api/_api.tsx
@@ -2,9 +2,12 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8001/api",
+    baseURL: "http://127.0.0.1:8001.api",
 });
 
+export const getCSRF = async () => {
+    await api.get("/csrf-token")
+};
 api.interceptors.request.use(config => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -12,7 +15,9 @@ api.interceptors.request.use(config => {
     }
     return config;
 }, error => {
+    console.log(error)
     return Promise.reject(error);
+    
 });
 
 export default api;

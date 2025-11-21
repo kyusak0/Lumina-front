944a38a2c069676cd9543782ce2a9e43cb159797
kyusak0 <chirkinvova4@gmail.com>
2025-11-21 14:56:30 +0300

    23

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import MainLayout from "./layouts/mainLayout"
 
 export default function NotFound() {
   return (
     <MainLayout>
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>404 - Страница не найдена</h1>
-      <p>Извините, мы не можем найти запрашиваемую страницу.</p>
-      <a href="/">Вернуться на главную</a>
+    <div className="text-center p-5 ">
+      <h1 className="text-4xl">404 - Страница не найдена</h1>
+      <p className="text-2xl">Извините, мы не можем найти запрашиваемую страницу.</p>
+      <Link href="/" className="underline">Вернуться на главную</Link>
     </div>
     </MainLayout>
   );
